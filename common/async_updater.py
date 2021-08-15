626a2ce01f515dd1d01b2bc0e1d7705905261c29
Junlin Huang <cricketlong@gmail.com>
2021-08-15 10:42:15 +0200

    use threading in AUpdater

--- common/async_updater.py
+++ common/async_updater.py
@@ -1,33 +1,53 @@
-import asyncio
+import threading
+import queue
+import time
 from data.TUData import TUData
 
 class AsyncUpdater():
     def __init__(self):
         self.num_updaters = 2
         self.td = TUData()
+        self.queue_lock = threading.Lock()
 
-    async def updater(self, updater_id, q):
-        while q.empty() == False:
-            date = await q.get()
-            q.task_done()
+    def updater(self, updater_id):
+        while True:
+            try:
+                self.queue_lock.acquire()
+                date = self.queue.get_nowait()
+                self.queue.task_done()
+                self.queue_lock.release()
 
-            await asyncio.sleep(1)
-            self.num_of_stocks_updated += self.td.update_daily(trade_date = str(date))
+                print("updater {0} got {1}".format(updater_id, date))
+                self.num_of_stocks_updated += self.td.update_daily(trade_date = str(date))
+                # time.sleep(5)
+                print("updater {0} finished updating {1}".format(updater_id, date))
+            except queue.Empty:
+                # queue is empty, exit thread.
+                print("Exception: queue empty")
+                self.queue_lock.release()
+                break
 
-    async def start_update(self, dates):
-        q = asyncio.Queue()
+    def start_update(self, dates):
+        self.finished = False
+        self.queue = queue.Queue()
         for date in dates:
-            await q.put(date)
+            self.queue.put(date)
 
-        updaters = [asyncio.create_task(self.updater(i, q)) for i in range(self.num_updaters)]
-        await asyncio.gather(*updaters)
-        await q.join()
+        updaters = list()
+        for i in range(self.num_updaters):
+            u = threading.Thread(target = self.updater, args = (i,))
+            updaters.append(u)
+            u.start()
+
+        self.queue.join()
+        # print("queue joined")
         for u in updaters:
-            u.cancel()
+            u.join()
+        # print("updaters joined")
 
     def update_all_stocks_on_dates(self, dates):
         self.num_of_stocks_updated = 0
-        asyncio.run(self.start_update(dates))
+        self.start_update(dates)
 
         return self.num_of_stocks_updated
 
