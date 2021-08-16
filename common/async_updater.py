056c165b367ce335b1e356b1b81e9b0c3994d488
Junlin Huang <cricketlong@gmail.com>
2021-08-17 00:33:17 +0200

    add call limit

--- common/async_updater.py
+++ common/async_updater.py
@@ -5,31 +5,71 @@ from data.TUData import TUData
 
 class AsyncUpdater():
     def __init__(self):
-        self.num_updaters = 2
+        self.num_updaters = 16
         self.td = TUData()
         self.queue_lock = threading.Lock()
+        self.call_ts_lock = threading.Lock()
+        self.set_call_limit(500, 60) # 500 calls/min
 
     def updater(self, updater_id):
         while True:
             try:
                 self.queue_lock.acquire()
-                date = self.queue.get_nowait()
+                date = self.queue.get_nowait() # will throw queue.Empty when self.queue is empty
                 self.queue.task_done()
                 self.queue_lock.release()
 
-                print("updater {0} got {1}".format(updater_id, date))
+                self.wait_for_available_call()
+
+                # print("updater {0} got {1}".format(updater_id, date))
                 self.num_of_stocks_updated += self.td.update_daily(trade_date = str(date))
                 # time.sleep(5)
-                print("updater {0} finished updating {1}".format(updater_id, date))
+                # print("updater {0} finished updating {1}".format(updater_id, date))
             except queue.Empty:
                 # queue is empty, exit thread.
-                print("Exception: queue empty")
+                # print("Exception: queue empty")
                 self.queue_lock.release()
                 break
 
+    def wait_for_available_call(self):
+        try:
+            self.call_ts_lock.acquire()
+            while self.call_timestamps.full():
+                # remove all time stamps older than self.n_seconds
+                sixty_seconds_ago = time.time() - self.n_seconds
+                timestamps_list = list(self.call_timestamps)
+                n = len(timestamps_list)
+                # print("timestamps queue size: " + n)
+                for i in range(n):
+                    t = timestamps_list[i]
+                    if t <= sixty_seconds_ago:
+                        # print("remove " + t)
+                        self.call_timestamps.get()
+                        self.call_timestamps.task_done()
+                    else:
+                        break
+
+                if self.call_timestamps.full() == False:
+                    break
+
+                time.sleep(1)
+
+            self.call_timestamps.put(time.time())
+            self.call_ts_lock.release()
+        except:
+            self.call_ts_lock.release()
+
+    def set_call_limit(self, ncalls, n_seconds):
+        """
+            call limit ncalls/n_seconds
+        """
+        self.ncalls = ncalls
+        self.n_seconds = n_seconds
+        self.call_timestamps = queue.Queue(self.ncalls)
+
     def start_update(self, dates):
-        self.finished = False
         self.queue = queue.Queue()
+
         for date in dates:
             self.queue.put(date)
 
